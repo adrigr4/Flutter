0cdb736eba76e775330264ff20ddf5ab159eb79b
Adrià Granero <50867803+adrigr4@users.noreply.github.com>
2020-02-18 13:30:24 +0100

    Lgos

--- FireBase/jomel-i6/lib/screens/authenticate/authenticate.dart
+++ FireBase/jomel-i6/lib/screens/authenticate/authenticate.dart
@@ -1,3 +1,4 @@
+import 'package:JomelI6/screens/authenticate/start.dart';
 import 'package:flutter/material.dart';
 import 'package:JomelI6/screens/authenticate/sign_in.dart';
 import 'package:JomelI6/screens/authenticate/register.dart';
@@ -9,16 +10,25 @@ class Authenticate extends StatefulWidget {
 
 class _AuthenticateState extends State<Authenticate> {
   bool showSignIn = true;
+  bool startPage = true;
   void toggleView() {
     setState(() => showSignIn = !showSignIn);
+    
+  }
+  void toggleStart() {
+    setState(() => startPage = !startPage);
   }
 
   @override
   Widget build(BuildContext context) {
+    if(startPage){
+      return Start(toggleStart: toggleStart);
+    }else{
     if (showSignIn) {
       return SignIn(toggleView: toggleView);
     } else {
       return Register(toggleView: toggleView);
+    }
     }
   }
 }
--- FireBase/jomel-i6/lib/screens/authenticate/start.dart
+++ FireBase/jomel-i6/lib/screens/authenticate/start.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class Start extends StatefulWidget {
 
+  final Function toggleStart;
+  Start({this.toggleStart});
+
   @override
   _StartState createState() => _StartState();
 }
@@ -11,11 +14,12 @@ class _StartState extends State<Start> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red[400],
-      body: Container(
+      body: Center(
         child: Column(
           children: <Widget>[
             SizedBox(height: 50.0),
-            Text('Jomel-I6'),
+            Text('JOMEL-I6 APP', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),),
+            Image.asset('assets/images/LOGO1.png'),
             RaisedButton(
               color: Colors.red[400],
               shape: RoundedRectangleBorder(
